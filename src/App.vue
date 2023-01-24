367bc8a66ffd00c23fd7ad6e1d2e72c8f0345e2d
MARIAM TSERTSVADZE <maricercvadze259@gmail.com>
2023-01-24 13:55:07 -0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,78 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script>
+
+export default{
+ data() {
+    return {
+      placeholder: '    search',
+        mylist: [ 
+           
+          "Home",
+          "Products",
+          "Contact us",
+          "info"
+       ]
+    }
+  }
+}
+
+</script> 
+
+
+
+<style>
+.container-fluid {
+  color: blueviolet;
+  margin-right:120px;
+  width: 100px;
+  font-size: 20px;
+  margin-top: 20px;
+  font-weight: 700;
+  margin-left: 40px;
+}
+.navbar {
+  display: flex;
+
+}
+h1{
+  margin-left: 350px;
+}
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+.nav{
+  background-color: darkgray;
+  height: 70px;
+}
+input{
+  height: 40px;
+  margin-top: 20px;
+  margin-left: 180px;
+  width: 180px;
+  border-radius: 20px;
+}
+
+</style>
+
+
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it! right?" />
+  
+    <div class="nav">
+        <nav class="navbar bg-dark" data-bs-theme="dark">
+        <a class="container-fluid" v-for="item in mylist">
+  {{ item }}
+        </a>
+        <input type="text"  :placeholder='placeholder'>
+  </nav>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
