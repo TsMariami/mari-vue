318913484fcc10b5ed8608734e3abb36c4c883e9
MARIAM TSERTSVADZE <maricercvadze259@gmail.com>
2023-01-26 02:08:59 -0800

    davaleba7

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,49 @@
 <script>
-
+import Menu from './list.vue'
 export default{
-
- data() {
+   components : {
+    Menu
+   },
+ data() { 
     return {
-      placeholder: '    search',
-        menulist: [ 
+        menulist: [
         {
             id: 1,
-            title: 'home',
-            URL : 'http://localhost:5173/'
+            title: 'Home',
+            URL : 'http://localhost:5173/',
+            description : 'description of "Home"',
+            
            },
            {
             id: 2,
-            title: 'products',
-            URL : 'http://localhost:5173/'
+            title: 'Products',
+            URL : 'http://localhost:5173/',
+            description : 'description of "Products"',
+            
            },
            {
             id: 3,
-            title: 'contact us',
-            URL : 'http://localhost:5173/'
+            title: 'Contact us',
+            URL : 'http://localhost :5173/',
+            description : 'description of "Contact us"'
            },
            {
             id: 4,
-            title: 'info',
-            URL : 'http://localhost:5173/'
-           },
-       
+            title: 'Info',
+            URL : 'http://localhost:5173/',
+            description : 'description of "Info"',
+        
+           },    
        ]
     }
-  }
+  },
 }
-
 </script> 
 
 
-
-<style>
-.container-fluid {
-  color: blueviolet;
-  margin-right:120px;
-  width: 100px;
-  font-size: 20px;
-  margin-top: 20px;
-  font-weight: 700;
-  margin-left: 40px;
-}
-li {
-  display: flex;
-
-}
-h1{
-  margin-left: 350px;
-}
-a,
-.green {
-  text-decoration: none;
-    transition: 0.4s;
-} 
-
-  @media (hover: hover) {
-  a:hover {
-    background-color: hsl(0, 96%, 69%);
-  }
-}
-
-.nav{
-  background-color: darkgray;
-  height: 70px;
-}
-input{
-  height: 40px;
-  margin-top: 20px;
-  margin-left: 180px;
-  width: 180px;
-  border-radius: 20px;
-}
-
-</style>
-
-
-
 <template>
-  
-    <div class="nav">
-        <nav>
-        <li>
-        <a :href= "item.URL" class="container-fluid" v-for= " item in menulist" :key="item.id">
-  {{ item.title }}
-        </a> 
-        <input type="text"  :placeholder='placeholder'></li>
-  </nav>
-    </div>
+  <Menu :menulist = "menulist"></Menu>
 </template>
+
+
+
---
+++ src/list.vue
@@ -0,0 +1,78 @@
+<script>
+export default{
+    props : { 
+      menulist: {type: Array, required: true}
+    },
+
+    data : function() { 
+       return {
+        show: true
+       }
+     },
+     methods: {
+      showorhide(x) {
+        this.show = !this.show
+        
+      }
+    
+      
+     },
+    }
+   </script> 
+   
+   
+   <template>
+     
+       <div class="nav">
+           <nav>
+           <div class="seconddiv" v-for= "item in menulist" :key="item.id">
+           <a  :href= "item.URL" @click.prevent="showorhide" >
+     {{ item.title }}
+           </a> 
+            <div v-if="show">{{ item.description }}</div> 
+    
+   </div>
+     </nav> 
+     </div>
+     <br>
+      <!-- <button  v-if="show">bla</button> -->
+   </template>
+   
+   
+   
+   <style>
+   .seconddiv {
+     color: blueviolet;
+     margin-right:120px;
+     width: 100px;
+     font-size: 20px;
+     margin-top: 20px;
+     margin-left: 40px;
+   }
+   nav{
+     display: flex;
+   
+   }
+   h1{
+     margin-left: 350px;
+   }
+   a,
+   .green {
+     text-decoration: none;
+       transition: 0.4s; 
+        font-weight: 700;
+   } 
+   
+     @media (hover: hover) {
+     a:hover {
+       background-color: hsl(0, 96%, 69%);
+     }
+   }
+   
+   .nav{
+     background-color: darkgray;
+     height: 70px;
+   }
+   
+   </style>
+   
