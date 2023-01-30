4b3758d52cdcb89a2c56d5022cd4efe387604cd9
MARIAM TSERTSVADZE <maricercvadze259@gmail.com>
2023-01-30 15:33:35 -0800

    01/30

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,88 @@
 <script>
-import additem from './additem.vue'
-import list from './list.vue'
-export default{
-  components: {
-     list ,
-     additem
-  },
-  data (){
-    return{
-      search: '',
-      todolist: [
-      {id: 1,
-        title:'to do item 1',
-        done: false
-        },
-       {id: 2,
-        title:'to do item 2',
-        done: false
-      } ,
-        {id: 3,
-        title:'to do item 3',
-        done: false
-        } 
-      ]
+    import axios from 'axios'
+
+    export default{
+      data() {
+        return {
+          range: [5, 10, 20],
+          limit: 5,
+          langs: ['ge', 'en'],
+          lang: 'ge',
+          apiUrl: "http://items.magischer.de/api/products",
+          products: [],
+          res: null,
+        }
+      },
+      methods: {
+    function(url = this.apiUrl){ 
+        axios.get(url, {
+             params: {
+             limit: this.limit,
+             lang: this.lang
+          }
+        }).then((response) =>{
+          this.res = response.data ,
+          this.products = response.data.data
+         })
+      }  ,
+         nextPage(){
+        this.function(this.res?.next_page_url)
+      },
+        previousPage(){
+        this.function(this.res?.prev_page_url)
+      },
+        firstPage(){
+        this.function(this.res?.first_page_url)
+      },
+      lastPage(){
+       this.function(this.res?.last_page_url)
+      },
+      changelang(e){  
+        this.lang = e.target.value
+        this.function()
+    },
+    changelimit(e){
+      this.limit = e.target.value
+        this.function()
     }
   },
-   computed: {
-    //  nextid(){
-    //   return this.todolist.lenght +1
-    //  } 
-    activeItems(){
-       return this.todolist.filter(item => item.done == false)
-    },
-    doneItems(){
-      return this.todolist.filter(item=> item.done == true)
-    }
-   },
-   filteredlist(){
-    return this.todolist.filter( item => {
-       return item.title.match(this.search);
-    } )
-
-   },
- methods: {
-    pushNewItemIntoList(data){
-      this.todolist.push({
-        id: 1,
-        title: data,
-        done: false
-
-      })
-     },
-    changeDone(record){
-      let x= this.todolist.find(item => item.id == record.id)
-      x.done = !x.done
-    },
-    deleteTodolistItem(record){
-      let x= this.todolist.find(item => item.id == record.id)
-       this.todolist.splice(this.todolist.indexOf(x), 1)
-    },
- 
-   
- },
-    
+  mounted() {
+    this.function()
+  },
 }
+  
 </script>
 
 <template>
-   <div class="left">
-    <additem  @onSubmit="pushNewItemIntoList($event)"/>
-    <h1>All items </h1>
-    <br>
-    <list :changeStatus="true" :data="todolist"
-    @changeDone="changeDone($event)"/>
-    <h1>Active items</h1>
-    <list  :data="activeItems" />
-    <h1>Done items</h1>
-    <list :delete="true"  :data="doneItems" @onDelete="deleteTodolistItem($event)" />
-    </div>
-
-    <div class="search">
-    <input type="text" v-model="search" placeholder="Search item" />
-    <div  v-for="item in filteredList" :key="item.index">
-    <h2>All items</h2> 
-    <list  :data="todolist"/>
-    </div>
-    </div>
+  <select @change="changelang">
+    <option v-for="language in langs" :key="language.index" :value="language" >
+    {{ language }} </option>
+  </select> 
+  <select @change="changelimit">
+    <option v-for="amount in range" :key="amount.index" :value="amount" >
+    {{ amount }} </option>
+  </select>
+  <table>
+    <thead>
+      <tr>
+        <th> porduct ID </th>
+        <th> product name </th>
+        <th> cover image </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in products" :key="item.index">
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }}</td>
+        <td><img :src="item.img.cover" alt=""></td>
+      </tr>
+    </tbody>
+  </table>
+  <a href="#" @click.prevent="firstPage" style="float : left" >first page</a>
+  <button style="float : left" :disabled="!res?.prev_page_url" @click.prevent="previousPage" > previous</button>
+   <button  :disabled="!res?.next_page_url" style="float : right" @click.prevent="nextPage">next</button>
+   <a href="#" @click.prevent="lastPage" style="float : right" >last page</a>
 </template>
-
-<style>
- .search{
-  margin-left: 710px;
-  margin-top: -380px;
- }
-.left{
-  display: inline-block;
-}
-</style>
 
  
 
