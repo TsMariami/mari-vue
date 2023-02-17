42620b0fa7201dba78f4be45ee228b8d28543382
MARIAM TSERTSVADZE <maricercvadze259@gmail.com>
2023-02-17 06:01:38 -0800

    2/17

---
+++ src/layout/apilayout.vue
@@ -0,0 +1,83 @@
+<script setup>
+import defineprops from '../props/defineprops';
+const { show } = defineprops()
+</script>
+<template >
+    <div>
+        <ul class=" flex flex-row justify-between">
+            <div class="inline-block flex flex-col mb-5 ">
+                <span class=" font-bold text-2xl text-blue-600 mb-4 text-center bg-gray-200 ">
+                    Name
+                </span>
+                <div class="inline-block flex flex-col mb-5 bg-gray-400" v-for="item in show" :key="item.index">
+                    {{ item.name }}
+                </div>
+            </div>
+            <div class="inline-block flex flex-col mb-5 ">
+                <span class=" font-bold text-2xl text-blue-600 mb-4 text-center bg-gray-300 ">
+                    Date
+                </span>
+                <div class="inline-block flex flex-col mb-5 bg-gray-500" v-for="item in show" :key="item.index">
+                    {{ item.date }}
+                </div>
+            </div>
+            <div class="inline-block flex flex-col mb-5 ">
+                <span class=" font-bold text-2xl text-blue-600 mb-4  bg-gray-200 ">
+                    Code
+                </span>
+                <div class="inline-block flex flex-col mb-5 text-center bg-gray-400" v-for="item in show" :key="item.index">
+                    {{ item.code }}
+                </div>
+            </div>
+            <div class="inline-block flex flex-col mb-5 ">
+                <span class=" font-bold text-2xl text-blue-600 mb-4 bg-gray-300 ">
+                    Quantity
+                </span>
+                <div class="inline-block flex flex-col mb-5 text-center bg-gray-500" v-for="item in show" :key="item.index">
+                    {{ item.quantity }}
+                </div>
+            </div>
+            <div class="inline-block flex flex-col mb-5 ">
+                <span class=" font-bold text-2xl text-blue-600 mb-4 bg-gray-200 ">
+                    RateFormated
+                </span>
+                <div class="inline-block flex flex-col mb-5 text-center bg-gray-400" v-for="item in show" :key="item.index">
+                    {{ item.rateFormated }}
+                </div>
+            </div>
+            <div class="inline-block flex flex-col mb-5 ">
+                <span class=" font-bold text-2xl text-blue-600 mb-4 bg-gray-300 ">
+                    DiffFormated
+                </span>
+                <div class="inline-block flex flex-col mb-5  text-center bg-gray-500" v-for="item in show"
+                    :key="item.index">
+                    {{ item.diffFormated }}
+                </div>
+            </div>
+            <div class="inline-block flex flex-col mb-5 ">
+                <span class=" font-bold text-2xl text-blue-600 mb-4 bg-gray-200">
+                    Rate
+                </span>
+                <div class="inline-block flex flex-col mb-5 bg-gray-400" v-for="item in show" :key="item.index">
+                    {{ item.rate }}
+                </div>
+            </div>
+            <div class="inline-block flex flex-col mb-5 ">
+                <span class=" font-bold text-2xl text-blue-600 mb-4 bg-gray-300">
+                    Diff
+                </span>
+                <div class="inline-block flex flex-col mb-5 bg-gray-500" v-for="item in show" :key="item.index">
+                    {{ item.diff }}
+                </div>
+            </div>
+            <div class="inline-block flex flex-col mb-5 ">
+                <span class=" font-bold text-2xl text-blue-600 mb-4 text-center bg-gray-200">
+                    ValidFromDate
+                </span>
+                <div class="inline-block flex flex-col mb-5 bg-gray-400" v-for="item in show" :key="item.index">
+                    {{ item.validFromDate }}
+                </div>
+            </div>
+        </ul>
+    </div>
+</template>
--- src/views/nbgComponent.vue
+++ src/views/nbgComponent.vue
@@ -1,8 +1,7 @@
 <script setup>
 import mainlayout from '../layout/mainlayout.vue';
-import defineprops from '../props/defineprops';
 import Menu from '../components/menu.vue';
-const { show } = defineprops()
+import apilayout from '../layout/apilayout.vue'
 </script>
 <template>
   <mainlayout>
@@ -10,80 +9,7 @@ const { show } = defineprops()
       <Menu />
     </template>
     <template #default>
-      <div class=" flex flex-row justify-between">
-        <div class="inline-block flex flex-col mb-5 ">
-          <span class=" font-bold text-2xl text-blue-600 mb-4 text-center bg-gray-200 ">
-            Name
-          </span>
-          <div class="inline-block flex flex-col mb-5 bg-gray-400" v-for="item in show" :key="item.index">
-            {{ item.name }}
-          </div>
-        </div>
-        <div class="inline-block flex flex-col mb-5 ">
-          <span class=" font-bold text-2xl text-blue-600 mb-4 text-center bg-gray-300 ">
-            Date
-          </span>
-          <div class="inline-block flex flex-col mb-5 bg-gray-500" v-for="item in show" :key="item.index">
-            {{ item.date }}
-          </div>
-        </div>
-        <div class="inline-block flex flex-col mb-5 ">
-          <span class=" font-bold text-2xl text-blue-600 mb-4  bg-gray-200 ">
-            Code
-          </span>
-          <div class="inline-block flex flex-col mb-5 text-center bg-gray-400" v-for="item in show" :key="item.index">
-            {{ item.code }}
-          </div>
-        </div>
-        <div class="inline-block flex flex-col mb-5 ">
-          <span class=" font-bold text-2xl text-blue-600 mb-4 bg-gray-300 ">
-            Quantity
-          </span>
-          <div class="inline-block flex flex-col mb-5 text-center bg-gray-500" v-for="item in show" :key="item.index">
-            {{ item.quantity }}
-          </div>
-        </div>
-        <div class="inline-block flex flex-col mb-5 ">
-          <span class=" font-bold text-2xl text-blue-600 mb-4 bg-gray-200 ">
-            RateFormated
-          </span>
-          <div class="inline-block flex flex-col mb-5 text-center bg-gray-400" v-for="item in show" :key="item.index">
-            {{ item.rateFormated }}
-          </div>
-        </div>
-        <div class="inline-block flex flex-col mb-5 ">
-          <span class=" font-bold text-2xl text-blue-600 mb-4 bg-gray-300 ">
-            DiffFormated
-          </span>
-          <div class="inline-block flex flex-col mb-5  text-center bg-gray-500" v-for="item in show" :key="item.index">
-            {{ item.diffFormated }}
-          </div>
-        </div>
-        <div class="inline-block flex flex-col mb-5 ">
-          <span class=" font-bold text-2xl text-blue-600 mb-4 bg-gray-200">
-            Rate
-          </span>
-          <div class="inline-block flex flex-col mb-5 bg-gray-400" v-for="item in show" :key="item.index">
-            {{ item.rate }}
-          </div>
-        </div>
-        <div class="inline-block flex flex-col mb-5 ">
-          <span class=" font-bold text-2xl text-blue-600 mb-4 bg-gray-300">
-            Diff
-          </span>
-          <div class="inline-block flex flex-col mb-5 bg-gray-500" v-for="item in show" :key="item.index">
-            {{ item.diff }}
-          </div>
-        </div>
-        <div class="inline-block flex flex-col mb-5 ">
-          <span class=" font-bold text-2xl text-blue-600 mb-4 text-center bg-gray-200">
-            ValidFromDate
-          </span>
-          <div class="inline-block flex flex-col mb-5 bg-gray-400" v-for="item in show" :key="item.index">
-            {{ item.validFromDate }}
-          </div>
-        </div>
-      </div>
+      <apilayout/>
     </template>
   </mainlayout>
-</template>      
+</template>     
